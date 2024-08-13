8b9e1dd434bd897f841323dce64344d66ad6041c
crissacm <alex.ccm7Z@gmail.com>
2024-08-13 11:22:47 -0400

    update

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const { dbConnection } = require('./database/config')
+const { dbConnection } = require('../database/config')
 
 const app = express()
 
@@ -9,8 +9,8 @@ dbConnection()
 app.use(cors())
 app.use(express.static('public'))
 app.use(express.json())
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/events', require('./routes/events'))
+app.use('/api/auth', require('../routes/auth'))
+app.use('/api/events', require('../routes/events'))
 
 app.listen(process.env.PORT, () => {
 	console.log(`Servidor corriendo en el puerto ${process.env.PORT}`)
